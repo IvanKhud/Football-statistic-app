87cf1ba28940cb4c841839519df805df060f76a0
Ivan Khud <anakin16@gmail.com>
2017-10-27 17:14:48 +0300

    created empty templates, added ngview, added routing config

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
-var app = angular.module('mainApp', []);
+var app = angular.module('mainApp', ["ngRoute"]);
 
 app.controller('mainCtrl', function($scope) {
 
+});
+
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/championships", {
+        templateUrl: 'templates/championships.template.html'
+    })
+    .when("/teams", {
+        templateUrl: 'templates/teams.template.html'
+    })
+    .when("/matches", {
+        templateUrl: 'templates/matches.template.html'
+    })
+    .when("/about", {
+        templateUrl: 'templates/about.template.html'
+    })
+    .otherwise({
+    	redirectTo: '/championships'
+    });
 });
