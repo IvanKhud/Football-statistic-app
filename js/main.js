40218532f4d5c6f24fbbfa801cac53d7fbfc320e
Ivan Khud <anakin16@gmail.com>
2017-10-29 18:22:40 +0200

    #3 #4 showed lists of teams and matches

--- js/main.js
+++ js/main.js
@@ -5,23 +5,13 @@ app.controller('mainCtrl', function($scope, $http) {
   .then(function(response) {
        $scope.championships = response.data.result;
    });
+  $http.get("https://footballbet.com.ua/api/teams/")
+  .then(function(response) {
+       $scope.teams = response.data.result;
+   });
+  $http.get("https://footballbet.com.ua/api/matches/")
+  .then(function(response) {
+       $scope.matches = response.data.result;
+   });
  });
 
-/*app.config(function($routeProvider) {
-    $routeProvider
-    .when("/championships", {
-        templateUrl: 'templates/championships.template.html'
-    })
-    .when("/teams", {
-        templateUrl: 'templates/teams.template.html'
-    })
-    .when("/matches", {
-        templateUrl: 'templates/matches.template.html'
-    })
-    .when("/about", {
-        templateUrl: 'templates/about.template.html'
-    })
-    .otherwise({
-    	redirectTo: '/championships'
-    });
-});*/
