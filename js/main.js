410cc890103581b3bf92527753f67c8d61c1db7a
Ivan Khud <anakin16@gmail.com>
2017-10-29 18:32:20 +0200

    created templates for matches, championships and teams

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ app.controller('mainCtrl', function($scope, $http) {
    });
  });
 
+app.config(function($routeProvider) {
+      $routeProvider
+      .when("/championships", {
+          templateUrl: 'templates/championships.template.html'
+     })
+     .when("/teams", {
+         templateUrl: 'templates/teams.template.html'
+     })
+     .when("/matches", {
+         templateUrl: 'templates/matches.template.html'
+     })
+     .when("/about", {
+         templateUrl: 'templates/about.template.html'
+     })
+      .otherwise({
+        redirectTo: '/championships'
+      });
+ });
