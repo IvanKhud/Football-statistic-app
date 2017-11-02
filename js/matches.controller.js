532308e391341161a890d57f2d9eff8512e8522e
Ivan Khud <anakin16@gmail.com>
2017-11-02 16:31:13 +0200

    #8 added ability to search

--- js/matches.controller.js
+++ js/matches.controller.js
@@ -3,10 +3,14 @@ app.controller('MatchesCtrl', function($http){
   
   $http.get("https://footballbet.com.ua/api/matches/")
   .then(function(response) {
-       arr.matches = response.data.result;
-       arr.totalItems = arr.matches.length;
+      arr.matches = response.data.result;
+      arr.totalItems = arr.matches.length;
+      for (var i = 0; i < arr.matches.length; i++) {
+      arr.matches[i].bothTeams = 
+      arr.matches[i].firstTeam.concat(" - ", arr.matches[i].secondTeam);    
+  };
   });
-
+  
   arr.viewby = 10;
   arr.currentPage = 4;
   arr.itemsPerPage = arr.viewby;
