a92488cfff69aea7681c2de00c88223e6497fc43
Ivan Khud <anakin16@gmail.com>
2017-11-05 09:20:48 +0200

    edited templates

--- js/match.controller.js
+++ js/match.controller.js
@@ -1,5 +1,5 @@
 app.controller('matchCtrl', ['$scope', '$location', 'dataFactory', function ($scope, $location, Data) {
-$scope.data = Data;
+  $scope.data = Data;
   var s = $location.path();
   var s1 = '';
   for (var i=9; i<s.length; i++) {s1 += s[i]};
