0d702bb62a5621336f5ef0679edef65f6c476dde
Ivan Khud <anakin16@gmail.com>
2017-11-09 00:47:56 +0200

    fixed table headers

--- js/matches.controller.js
+++ js/matches.controller.js
@@ -2,6 +2,8 @@ app.controller('matchesCtrl', ['$scope', 'dataFactory', function ($scope, Data) 
 
   $scope.data = Data;
   $scope.data.getMatches();
+  $scope.data.getTeams();
+  $scope.data.getChampionships();
   $scope.currentPage = 1;
 
 }]);
--- js/teams.controller.js
+++ js/teams.controller.js
@@ -2,6 +2,7 @@ app.controller('teamsCtrl', ['$scope', 'dataFactory', function ($scope, Data) {
 
   $scope.data = Data;
   $scope.data.getTeams();
+  $scope.data.getChampionships();
   $scope.currentPage = 1;
 
 }]);
